e94f52940b9e8a48ae15d4d6e0bed80218f1b3f7
Eli <elisabeth.ericson@gmail.com>
2022-04-10 22:01:02 -0400

    Draft table schema for Postgres database

---
+++ sql/table_creation_query.sql
@@ -0,0 +1,22 @@
+/* 
+DRAFT TABLE CREATION QUERY 
+2022-04-10 
+
+Roughly based on GBFS free_bike_status.json schema,
+with some version differences between providers.
+See https://github.com/MobilityData/gbfs-json-schema
+and District of Columbia Department of Transportation
+dockless APIs https://ddot.dc.gov/page/dockless-api
+*/
+
+CREATE TABLE vehicles (
+	provider TEXT NOT NULL,
+	bike_id TEXT NOT NULL,
+	vehicle_type TEXT,
+	is_reserved BOOL,
+	is_disabled BOOL,
+	battery_level INT,
+	lat DECIMAL(10, 8) NOT NULL,
+	lon DECIMAL(11, 8) NOT NULL,
+	time_scraped TIMESTAMPTZ NOT NULL
+);
